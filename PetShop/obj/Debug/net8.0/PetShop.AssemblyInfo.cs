2dc022981c3d9b6a0968486ed601219e46b1988e
Gs <gs3113857@gmail.com>
2025-09-14 13:05:38 -0300

    v1.0

--- PetShop/obj/Debug/net8.0/PetShop.AssemblyInfo.cs
+++ PetShop/obj/Debug/net8.0/PetShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68aeed56d932c8d6eac93bf281c14c61535c5776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a1b6df2e54ab6dea6d5ffab6593cb36eafb423")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
