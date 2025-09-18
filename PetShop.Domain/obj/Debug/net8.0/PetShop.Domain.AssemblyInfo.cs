da7a9ee39d6b2474fbcfab4837dea1e5b82ff17a
Gs <gs3113857@gmail.com>
2025-09-17 23:47:57 -0300

    v1.2

--- PetShop.Domain/obj/Debug/net8.0/PetShop.Domain.AssemblyInfo.cs
+++ PetShop.Domain/obj/Debug/net8.0/PetShop.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShop.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c5dc6fcfa750288c0baf90d0f5ca1df64499125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf84265f9a4619f9fbb0f0ed8a9764c937013cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShop.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShop.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
