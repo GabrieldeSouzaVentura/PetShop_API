9815e09da3a1dd6a55a31ff0491f9313f7fba9a8
Gs <gs3113857@gmail.com>
2025-09-17 23:47:33 -0300

    Add new services

---
+++ PetShop.Application/Service/IService/ITutorServices.cs
@@ -0,0 +1,14 @@
+﻿using PetShop.DTOs;
+using PetShop.DTOs.TutorDtos;
+
+namespace PetShop.Application.Service.IService;
+
+public interface ITutorServices
+{
+    Task<ResponseTutorDto> RegisterTutor(RegisterTutorDto registerTutorDto);
+    Task<ResponseTutorDto> GetByNameTutor(string name);
+    Task<ResponseTutorDto> GetAllInformationsTutor();
+    Task<UpdateTutorDto> UpdateTutor(int id, UpdateTutorDto updateTutorDto);
+    Task Delete(int id);
+
+}
---
+++ PetShop.Application/Service/TutorServises.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using PetShop.Application.Service.IService;
+using PetShop.DTOs;
+using PetShop.DTOs.TutorDtos;
+using PetShop.Repositories.IRepositories;
+
+namespace PetShop.Application.Service;
+
+public class TutorServises : ITutorServices
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public async Task Delete(int id)
+    {
+        var tutor = await _unitOfWork.TutorRepository.GetAsync(t => t.TutorId == id, i => i.Include(p => p.Pets));
+
+        if (tutor == null) throw new Exception("Tutor not found");
+    }
+
+    public Task<ResponseTutorDto> GetAllInformationsTutor()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseTutorDto> GetByNameTutor(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseTutorDto> RegisterTutor(RegisterTutorDto registerTutorDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UpdateTutorDto> UpdateTutor(int id, UpdateTutorDto updateTutorDto)
+    {
+        throw new NotImplementedException();
+    }
+}
