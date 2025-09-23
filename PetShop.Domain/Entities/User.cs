628bee25a0d8daed9861b50f042eb934dfbde2d4
Gs <gs3113857@gmail.com>
2025-09-22 21:43:41 -0300

    New user entity

---
+++ PetShop.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using PetShop.Models;
+
+namespace PetShop.Domain.Entities;
+
+public class User
+{
+    [JsonIgnore]
+    public int UserId { get; set; }
+    public int TutorId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string? Role { get; set; }
+    [JsonIgnore]
+    public Tutor Tutor { get; set; }
+}
