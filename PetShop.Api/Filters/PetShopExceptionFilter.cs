1d50c404276b2d1edfc4de7aa44eef7642a479f1
Gs <gs3113857@gmail.com>
2025-09-28 15:55:27 -0300

    Update exception filter

--- PetShop.Api/Filters/PetShopExceptionFilter.cs
+++ PetShop.Api/Filters/PetShopExceptionFilter.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using PetShop.Application.Service.Exceptions;
 
 namespace PetShop.PetShop.Api.Filters;
 
@@ -14,10 +16,25 @@ public class PetShopExceptionFilter : IExceptionFilter
 
     public void OnException(ExceptionContext context)
     {
-        _logger.LogError(context.Exception, "An unhandled exception occurred: Status code 500");
-        context.Result = new ObjectResult("There was a problem processing your request: Status code 500")
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        string errorMessage = "There was a problem processing your request: Status code 500";
+
+        var exceptionType = context.Exception.GetType();
+
+        if (exceptionType == typeof(ResourceNotFoundException))
         {
-            StatusCode = StatusCodes.Status500InternalServerError,
+            errorMessage = context.Exception.Message;
+            statusCode = HttpStatusCode.NotFound;
+        } else if (exceptionType == typeof(BusinessValidationException))
+        {
+            errorMessage = context.Exception.Message;
+            statusCode = HttpStatusCode.BadRequest;
+        }
+
+        _logger.LogError(context.Exception, "An unhandled exception occurred: Status code 500");
+        context.Result = new ObjectResult(errorMessage)
+        {
+            StatusCode = (int)statusCode
         };
     }
 }
